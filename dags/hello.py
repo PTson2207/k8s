047e24ade71ce583ff742e4bd2d8416ec887e90e
PTson2207 <sonphukl@gmail.com>
2025-01-01 15:24:59 +0700

    first commit

---
+++ dags/hello.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# Define the DAG configuration
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime.datetime.now(),
+    'catchup': False,
+}
+
+with DAG('hello', default_args=default_args, schedule_interval='@daily') as dag:
+    # Define the tasks
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+    )
+
+    t2 = BashOperator(
+        task_id='sleep',
+        bash_command='sleep 5',
+    )
+
+    t3 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "Hello World"',
+    )
+
+# Define the task dependencies
+t1 >> t2 >> t3
